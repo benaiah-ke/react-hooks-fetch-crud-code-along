a595878a38269cce027be3468780be86314bc767
benaiah-ke <benaiahpraise@gmail.com>
2022-09-27 07:52:07 +0300

    Done with lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
